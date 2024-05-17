677a6297691f27e878a7fe4cc7f5c60df0bf78c0
Maybe Ai? <Nirt_12023@outlook.com>
2024-05-17 15:17:48 +0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="CHANGE ME",
+    version="CHANGE ME",
+    packages=find_packages(),
+    description="CHANGE ME",
+    author="CHANGE ME",
+    author_email="CHANGE ME",
+    url="https://github.com/DefinetlyNotAI/IG_Python_Library",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+    keywords="igcse computer science",
+    install_requires=[
+        # List your project's dependencies here.
+    ],
+    extras_require={
+        # Optional dependencies
+    },
+    python_requires='>=3.6',
+)
